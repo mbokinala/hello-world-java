84d2c0a049a1dd56f86d2b266e6dcc97a8983bb2
Manav Bokinala <manav.bokinala@gmail.com>
2017-10-06 14:27:16 -0700

    Added JOptionPane

--- src/com/mbokinala/helloworld/HelloWorld.java
+++ src/com/mbokinala/helloworld/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.mbokinala.helloworld;
 
+import javax.swing.*;
+
 public class HelloWorld {
 
     public static void main(String args[]) {
         System.out.println("Hello World");
+
+        JOptionPane.showMessageDialog(new JFrame(), "Hello World!");
     }
 
 }
